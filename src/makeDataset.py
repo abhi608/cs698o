558fc6979f73ae39674e2da822765704da38f03e
Abhishek <608abhishek@gmail.com>
2017-09-12 02:31:15 +0530

    latest makedatasetr

--- src/makeDataset.py
+++ src/makeDataset.py
@@ -32,34 +32,35 @@ test_classes = train_classes
 
 for dir in folders:
 	print dir
-	raw_image = '../datasets/'+dir+'/VOC2007/JPEGImages/'
-	anno_image = '../datasets/'+dir+'/VOC2007/Annotations/'
-	train_dataset = '../datasets/train_final/'
+	raw_image = '../datasets/'+dir+'/JPEGImages/'
+	anno_image = '../datasets/'+dir+'/Annotations/'
+	train_dataset = '../datasets/train/final/'
 	for anno_filename in os.listdir(anno_image):
 		tree = 	ET.parse(anno_image+anno_filename)
 		tmp = anno_filename.split('.')
 		img_filename = tmp[0] + '.jpg'
-		print anno_filename, img_filename
+		# print anno_filename, img_filename
 		img = cv2.imread(raw_image+img_filename)
 		root = tree.getroot()
 		print root
 		for i,object in enumerate(root.findall('object')):
 			class1 = str(object.find('name').text)
 			bndbox = object.find('bndbox')
-			print class1, bndbox[0].text, bndbox[1].text, bndbox[2].text, bndbox[3].text
+			# print class1, bndbox[0].text, bndbox[1].text, bndbox[2].text, bndbox[3].text
 			class1 = str(object[0].text)
 			xmin = int(bndbox[0].text)
 			ymin = int(bndbox[1].text)
 			xmax = int(bndbox[2].text)
 			ymax = int(bndbox[3].text)
-			crop_img = img[ymin:ymax, xmin:xmax]
-			if dir == 'train':
-				cv2.imwrite(train_dataset+class1+'/'+tmp[0]+'_'+str(i)+'.jpg', crop_img)
-				train_classes[class1].append(crop_img)
+			print ymax-ymin, xmax-xmin
+			# crop_img = img[ymin:ymax, xmin:xmax]
+			# if dir == 'train':
+			# 	cv2.imwrite(train_dataset+class1+'/'+tmp[0]+'_'+str(i)+'.jpg', crop_img)
+			# 	train_classes[class1].append(crop_img)
 			# else:
 			# 	test_classes[class1].append(crop_img)
 		# break
 
-print train_classes, test_classes
+# print train_classes, test_classes
 
 
